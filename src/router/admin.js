52443d77f4d3156216b5ff82f57715492d87d5c8
Gazi Mehedi Hasan <gazimehedihasan512@gmail.com>
2024-01-13 04:27:39 +0600

    admin user create page design done

--- src/router/admin.js
+++ src/router/admin.js
@@ -1,5 +1,6 @@
 
 import Dashboard from "@/views/admin/Dashboard.vue";
+import UserCreatePage from "@/views/admin/user/Create.vue";
 import UserListPage from "@/views/admin/user/UsersList.vue";
 
 export default [
@@ -10,6 +11,10 @@ export default [
     {
         path: "users-list",
         component: UserListPage,
+    },
+    {
+        path: "user-create",
+        component: UserCreatePage,
     },
     {
         path: "settings",
